88436bd9bcfdb0d73c9bbf7442aa88c739866a5a
Olatunji93 <tunjioguntoyinboahf@gmail.com>
2022-09-12 20:09:49 -0700

    respond

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints the lowercase alphabet in reverse 
+ * main - Prints the lowercase alphabet in reverse
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
