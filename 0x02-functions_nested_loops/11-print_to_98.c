2c6e6b66179214e8bed41ea86d14b47c7b3f2fde
Olatunji93 <tunjioguntoyinboahf@gmail.com>
2022-09-14 16:19:37 -0700

    retro

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,5 +14,8 @@ void print_to_98(int n)
 	if (n > 98)
 		for (count = n; count > 98; --count)
 			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
 	print("98\n");
 }
