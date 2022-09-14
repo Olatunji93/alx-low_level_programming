b5719637339aa90b7aed8bbd70f0ff8b40e1651e
Olatunji93 <tunjioguntoyinboahf@gmail.com>
2022-09-14 11:22:45 -0700

    dj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * Description - Prints alphabets to output
+ * Description - Prints alphabet to output
  */
-void print_alphabets(void)
+void print_alphabet(void)
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
+	return (1);
 }
