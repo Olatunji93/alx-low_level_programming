9e0fc1d4768dcb5afb80b0d2bcc0b9b28242d95d
Olatunji93 <tunjioguntoyinboahf@gmail.com>
2022-09-14 15:59:46 -0700

    troy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		_putchar(48);
 		for (mult = 1; mult <= 9; ++mult)
 		{
-			_putchar(' ');
+			_putchar('.');
 			_putchar(' ');
 			prod = num * mult;
 			/*
@@ -26,7 +26,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 0); /* to get the first digit */
+				_putchar((prod / 10) + 0); /* get the first digit */
 			_putchar((prod % 10) + 0); /* get the second digit */
 		}
 		_putchar('\n');
