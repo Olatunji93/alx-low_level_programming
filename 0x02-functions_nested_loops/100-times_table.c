68e799396bf3bf761f425d8b3d91b8d4aeeb41ce
Olatunji93 <tunjioguntoyinboahf@gmail.com>
2022-09-14 20:47:24 -0700

    gut

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int prod, mult, num;
 
-	if (n <= 15 && n => 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; ++num)
 		{
@@ -23,7 +23,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
+					_putchar((prod % 100) + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
